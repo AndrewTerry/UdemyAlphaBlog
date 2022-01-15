6072aa85f00d6dcf2df090b260c299dc417cc10b
Andrew Terry <andrew@andrewterry.com>
2022-01-15 18:27:13 +0000

    Add Bootstrap and some terrible styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,26 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "../stylesheets/application"
+
+require("@popperjs/core")
+
+// Import the specific modules you may need (Modal, Alert, etc)
+import { Tooltip, Popover } from "bootstrap"
+
+document.addEventListener("turbolinks:load", () => {
+    // Both of these are from the Bootstrap 5 docs
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+})
 
 Rails.start()
 Turbolinks.start()
