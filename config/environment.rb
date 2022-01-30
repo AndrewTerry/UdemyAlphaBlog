45a4418e68196b71cdb1ca87773afc84fd545719
Andrew Terry <andrew@andrewterry.com>
2022-01-30 14:51:01 +0000

    Correctly display footer on all pages; add styling to Show page

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Override default field_with_errors behaviour
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
