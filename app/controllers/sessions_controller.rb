c43cfe5d94714eb47f1e177f9608524021b44fbc
Andrew Terry <andrew@andrewterry.com>
2022-05-16 07:24:04 +0000

    add create/destory sessions for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "User logged in successfully!"
+      redirect_to user
+    else
+      flash.now[:alert] = "Username or password is incorrect"
+      render 'new'
+    end
   end
   
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "User logged out"
+    redirect_to root_path
   end
   
 
